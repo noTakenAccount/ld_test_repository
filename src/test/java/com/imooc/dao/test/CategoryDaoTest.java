bb4464f92a308f2fcaae8d3331f3be5d31b92c72
noTakenAccount <403364404@qq.com>
2020-04-19 18:24:01 +0800

    sixth cart commit

---
+++ src/test/java/com/imooc/dao/test/CategoryDaoTest.java
@@ -0,0 +1,36 @@
+package com.imooc.dao.test;
+
+import com.imooc.test.TestBase;
+import com.mmall.dao.CategoryMapper;
+import com.mmall.pojo.Category;
+import com.mmall.service.impl.CategoryServiceImpl;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Created by geely
+ */
+public class CategoryDaoTest extends TestBase {
+
+
+    @Autowired
+    private CategoryMapper categoryMapper;
+    @Autowired
+    private CategoryServiceImpl iCategoryService;
+
+    @Ignore
+    @Test
+    public void getCategoryChild(){
+        Category d  = categoryMapper.selectByPrimaryKey(1);
+        System.out.println(d);
+        Category d4  = categoryMapper.selectByPrimaryKey(4);
+        System.out.println(d4);
+    }
+    @Test
+    public void testChildService(){
+        iCategoryService.selectCategoryAndChildrenById(2);
+    }
+
+}
---
+++ src/test/java/com/imooc/test/BigDecimalTest.java
@@ -0,0 +1,40 @@
+package com.imooc.test;
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by geely
+ */
+
+
+
+
+public class BigDecimalTest {
+
+    @Test
+    public void test1(){
+        System.out.println(0.05+0.01);
+        System.out.println(1.0-0.42);
+        System.out.println(4.015*100);
+        System.out.println(123.3/100);
+    }
+
+    @Test
+    public void test2(){
+        BigDecimal b1 = new BigDecimal(0.05);
+        BigDecimal b2 = new BigDecimal(0.01);
+        System.out.println(b1.add(b2));
+    }
+
+    @Test
+    public void test3(){
+        //使用要使用 string构造器  否则出问题
+        BigDecimal b1 = new BigDecimal("0.05");
+        BigDecimal b2 = new BigDecimal("0.01");
+        System.out.println(b1.add(b2));
+
+    }
+
+}
